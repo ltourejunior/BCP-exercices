319ede6c82cd45864c1b3de783959a060dd247f0
Unknown <m.toure593@gmail.com>
2017-01-19 13:43:43 +0100

    Day 9 mid day

--- day8/js/jspractice.js
+++ day8/js/jspractice.js
@@ -1,1 +1,6 @@
-output.html('input element')
+$('.output').css('fontsize', '50px');
+$('.button').click(function(){
+  // $(this) est l'équivalent de refaire $('.button') :
+  // en gros, il reprend l'élément sur lequel on applique une fonction
+  alert('this is new');
+});
